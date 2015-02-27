c32b7cc9e1f551a95431b1284968d3baa7da67a4
kirillov <kirillov@google.com>
2015-02-27 10:13:05 +0000

    Removing run scripts

--- examples/emm-notifications/run-subscriber.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mvn clean compile assembly:single && \
-java -cp target/emm-notifications-1.0-SNAPSHOT-jar-with-dependencies.jar \
-     com.google.android.work.emmnotifications.PushSubscriber  \
-        --subscription_name=projects/enterprise-cloud-pub-sub/subscriptions/hello-world-push-subscription \
-        --topic_name=projects/enterprise-cloud-pub-sub/topics/hello-world-topic
